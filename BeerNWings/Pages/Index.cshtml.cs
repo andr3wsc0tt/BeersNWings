e3c4b4698962039941e9cfb8a0b810da68f7fc27
Your Name <you@example.com>
2020-05-20 06:57:48 -0600

    connected to db

---
+++ BeerNWings/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BeerNWings.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BeerNWings.Pages {
+    public class CoursesAndModulesModel : PageModel {
+        DataAccessController dac = new DataAccessController ();
+
+        public List<CoursesAndModules> CoursesAndModules;
+
+        public void OnGet () {
+            CoursesAndModules = dac.GetAllCoursesAndModules ().ToList ();
+        }
+    }
+}
